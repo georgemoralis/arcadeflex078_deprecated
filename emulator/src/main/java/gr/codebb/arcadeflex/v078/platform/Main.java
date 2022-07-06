d6394dc3d904412dab2c9ce8e739bcb0956a586b
Chusogar <chusogar@gmail.com>
2022-07-06 15:50:02 +0200

    added project skeleton

---
+++ emulator/src/main/java/gr/codebb/arcadeflex/v078/platform/Main.java
@@ -0,0 +1,8 @@
+package gr.codebb.arcadeflex.v078.platform;
+
+public class Main {
+    public static void main(String[] args) {
+/*TODO*///        ConvertArguments("arcadeflex", args);
+/*TODO*///        System.exit(osdepend.main(argc, argv));
+    }    
+}
